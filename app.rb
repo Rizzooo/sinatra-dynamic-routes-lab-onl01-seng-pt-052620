72321c7f8065d9c8980458253ca012479eef1938
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-19 15:05:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,6 +34,12 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
   
-  
+  get '/:operation/:number1/:number2' do 
+    @op = params[:operation]
+    @num1 = params[:number1]
+    @num2 = params[:number2]
+    
+    if @op == 
+  end
   
 end
