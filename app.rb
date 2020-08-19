4a8a93aebbc390fad82ecd3748d0e7148447940b
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-19 14:41:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,12 +4,11 @@ class App < Sinatra::Base
   
   get '/reversename/:name' do
     @name = params[:name].reverse!
-    
     "#{@name}"
   end
   
   get '/square/:number' do 
-    @squared = params[:number].to_i
+    @squared = params[:number].to_i *
   end
 
 end
