6e0a3be42260600d7bafd60e9544ea58637b1064
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-19 14:34:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/reversename/:name'
     name = params[:name]
+    @flip = name.reverse
     
-    @flip = name.
+    "#{@flip}"
   end
 
 end
